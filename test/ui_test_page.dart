c360065e424d36a005c31d525238b5b0ea79deb4
KeHong Wang <dreamrenderx@gmail.com>
2024-02-04 18:15:29 +0800

    feat: google login test

--- test/ui_test_page.dart
+++ test/ui_test_page.dart
@@ -1,6 +1,25 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mathgptpro_mclient_flutter/component/bottom_sheet/upgrade_plan_bottom_sheet.dart';
+
+void main() {
+  runApp(const Example());
+}
+
+class Example extends StatelessWidget {
+  const Example({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: UiTestPage(),
+    );
+  }
+}
 
 class UiTestPage extends StatefulWidget {
   const UiTestPage({super.key});
@@ -10,6 +29,29 @@ class UiTestPage extends StatefulWidget {
 }
 
 class _UiTestPageState extends State<UiTestPage> {
+  Future<void> signinWithGoogle() async {
+    const GOOGLE_CLIENT_DEV_KEY =
+        '1074402275475-27sj1mpskdamepksb4nftihk60ndfg07.apps.googleusercontent.com';
+    final GoogleSignIn googleSignIn = GoogleSignIn(
+      clientId: GOOGLE_CLIENT_DEV_KEY,
+      scopes: [
+        'https://www.googleapis.com/auth/userinfo.email',
+        'openid',
+        'https://www.googleapis.com/auth/userinfo.profile',
+      ],
+    );
+    try {
+      final googleUserAccount = await googleSignIn.signIn();
+      final googleAuth = await googleUserAccount?.authentication;
+      if (googleAuth != null) {
+        log(googleAuth.idToken.toString());
+        print(googleAuth.accessToken.toString());
+      }
+    } catch (error) {
+      print(error);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
