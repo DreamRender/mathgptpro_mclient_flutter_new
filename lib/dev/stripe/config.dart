9d964795038b999225ed1c40adda33504e8c52df
KeHong Wang <dreamrender@truezir.com>
2024-02-06 11:00:37 +0800

    Revert "feat: stripe apple pay"
    This reverts commit 0d77b7ae0b19aa10d675dac6a02419bed559c8e7.

--- lib/dev/stripe/config.dart
+++ lib/dev/stripe/config.dart
@@ -4,4 +4,4 @@ import 'package:flutter/foundation.dart';
 // with the endpoint of your test server (it usually should be the IP of your computer)
 final kApiUrl = defaultTargetPlatform == TargetPlatform.android
     ? 'http://mathgptpro.ihk.fghk.top'
-    : 'http://mathgptpro.ihk.fghk.top';
+    : 'http://localhost:4242';
